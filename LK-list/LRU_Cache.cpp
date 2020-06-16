7cff76f2f53e672ce3d821377f2a4e08924fe04c
Siddhant saxena <66401356+mr-siddy@users.noreply.github.com>
2020-06-16 20:09:08 +0530

    extra comment eradicated

--- LK-list/LRU_Cache.cpp
+++ LK-list/LRU_Cache.cpp
@@ -1,11 +1,3 @@
-// We can use stl container list as a double 
-// ended queue to store the cache keys, with 
-// the descending time of reference from front 
-// to back and a set container to check presence 
-// of a key. But to fetch the address of the key 
-// in the list using find(), it takes O(N) time. 
-// This can be optimized by storing a reference 
-//	 (iterator) to each key in a hash map. 
 #include <bits/stdc++.h> 
 using namespace std; 
 
@@ -84,4 +76,4 @@ int main()
 
 	return 0; 
 } 
-// This code is contributed by Satish Srinivas 
+
